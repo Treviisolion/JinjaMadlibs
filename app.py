ff6e322df6fdd380ea92c461be6f31b627b98935
Trevyn <trevyncase@gmail.com>
2020-10-26 10:52:50 -0500

    Create Project

---
+++ app.py
@@ -0,0 +1,16 @@
+from stories import story
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def get_form():
+    return render_template("index.html", prompts = story.prompts)
+
+@app.route("/story")
+def return_story():
+    return render_template("story.html", story = story.generate(request.args))
