489ad3be5d4174276c5842f00d53fe5dd1fde929
Trevyn <trevyncase@gmail.com>
2020-10-30 18:20:47 -0500

    minor aesthetic change

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from stories import story
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
